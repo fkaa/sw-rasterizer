9b8e12b9bbe9cb0f667015feb000f47761a58338
Felix Kaaman <trundmatu@gmail.com>
2016-11-20 20:18:07 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,220 @@
+#![feature(slice_patterns)]
+#![feature(step_by)]
+
+#[macro_use]
+extern crate gfx;
+extern crate gfx_app;
+extern crate cgmath;
+extern crate obj;
+extern crate hprof;
+
+use cgmath::SquareMatrix;
+use cgmath::{Matrix4, Vector4, Vector3, Vector2, Transform, Point3};
+
+use gfx::{Bundle};
+use gfx::texture::Kind;
+use gfx::format::{Rgba8, Depth, ChannelType};
+use gfx::memory::{Usage, Access};
+
+use gfx_app::ColorFormat;
+
+use std::path::Path;
+use std::time::Instant;
+
+mod rasterizer;
+
+use rasterizer::*;
+
+#[derive(Copy, Clone)]
+pub struct Color(Vector3<f32>);
+#[derive(Copy, Clone)]
+pub struct Uv(Vector2<f32>);
+
+impl Blend for Color {
+    fn blend(a: Self, aw: f32, b: Self, bw: f32, c: Self, cw: f32) -> Self {
+        Color(Vector3::new(a.0.x * aw + b.0.x * bw + c.0.x * cw,
+                           a.0.y * aw + b.0.y * bw + c.0.y * cw,
+                           a.0.z * aw + b.0.z * bw + c.0.z * cw))
+    }
+}
+
+impl Blend for Uv {
+    fn blend(a: Self, aw: f32, b: Self, bw: f32, c: Self, cw: f32) -> Self {
+        Uv(Vector2::new(a.0.x * aw + b.0.x * bw + c.0.x * cw,
+                        a.0.y * aw + b.0.y * bw + c.0.y * cw))
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Vertex {
+    pos: Vector3<f32>,
+    uv: Vector2<f32>
+}
+
+pub fn vertex_shader(input: Vertex, mat: Matrix4<f32>) -> (Uv, Vector4<f32>) {
+    (Uv(input.uv), mat * input.pos.extend(1f32))
+}
+
+pub fn fragment_shader(fragment: Vector2<f32>, input: Uv) -> Vector4<f32> {
+    Vector4::new(input.0.x, input.0.y, 1f32, 1f32)
+}
+
+
+const SIZE: (u32, u32) = (854, 640);
+
+gfx_defines!{
+    vertex BlitVertex {
+        pos: [f32; 2] = "a_Pos",
+        uv: [f32; 2] = "a_Uv",
+    }
+
+    pipeline blit {
+        vbuf: gfx::VertexBuffer<BlitVertex> = (),
+        blit: gfx::TextureSampler<[f32; 4]> = "t_Blit",
+        out: gfx::RenderTarget<gfx::format::Rgba8> = "Target0",
+    }
+}
+
+impl BlitVertex {
+    pub fn new(pos: [f32; 2], uv: [f32; 2]) -> Self {
+        BlitVertex {
+            pos: pos,
+            uv: uv,
+        }
+    }
+}
+
+struct App<R: gfx::Resources> {
+    bundle: Bundle<R, blit::Data<R>>,
+    blit_texture: gfx::handle::Texture<R, gfx::format::R8_G8_B8_A8>,
+    rasterizer: Rasterizer,
+    pipeline: Pipeline<Vertex, Uv>,
+    model: Vec<Vertex>,
+    start_time: Instant,
+    prof: hprof::Profiler
+}
+
+impl<R> gfx_app::Application<R> for App<R>
+        where R: gfx::Resources
+{
+    fn new<F: gfx::Factory<R>>(mut factory: F, init: gfx_app::Init<R>) -> Self {
+        use gfx::traits::FactoryExt;
+
+        let vs = gfx_app::shade::Source {
+            glsl_150: include_bytes!("../shader/blit.glslv"),
+            .. gfx_app::shade::Source::empty()
+        };
+        let ps = gfx_app::shade::Source {
+            glsl_150: include_bytes!("../shader/blit.glslf"),
+            .. gfx_app::shade::Source::empty()
+        };
+
+        let vertex_data = [
+            BlitVertex::new([-1.0, -1.0], [0.0, 1.0]),
+            BlitVertex::new([ 1.0, -1.0], [1.0, 1.0]),
+            BlitVertex::new([ 1.0,  1.0], [1.0, 0.0]),
+
+            BlitVertex::new([-1.0, -1.0], [0.0, 1.0]),
+            BlitVertex::new([ 1.0,  1.0], [1.0, 0.0]),
+            BlitVertex::new([-1.0,  1.0], [0.0, 0.0]),
+        ];
+
+        let (vbuf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, ());
+
+        let texture = factory.create_texture(Kind::D2(SIZE.0 as _, SIZE.1 as _, gfx::texture::AaMode::Single), 1, gfx::SHADER_RESOURCE, Usage::CpuOnly(gfx::memory::WRITE), Some(ChannelType::Unorm)).unwrap();
+        let view = factory.view_texture_as_shader_resource::<gfx::format::Rgba8>(&texture, (0, 0), gfx::format::Swizzle::new()).unwrap();
+
+        let sampler = factory.create_sampler_linear();
+
+        let pso = factory.create_pipeline_simple(
+            vs.select(init.backend).unwrap(),
+            ps.select(init.backend).unwrap(),
+            blit::new()
+        ).unwrap();
+
+        let data = blit::Data {
+            vbuf: vbuf,
+            blit: (view.clone(), sampler.clone()),
+            out: init.color,
+        };
+
+        App {
+            bundle: Bundle::new(slice, pso, data),
+            blit_texture: texture,
+
+            rasterizer: Rasterizer::new(SIZE.0 as _, SIZE.1 as _),
+            pipeline: Pipeline::new(vertex_shader, fragment_shader),
+            model: {
+                let object = obj::load::<obj::SimplePolygon>(Path::new("./data/spot.obj")).unwrap();
+                let indices = object.object_iter().next().unwrap().group_iter().next().unwrap().indices();
+
+                let mut vertices = Vec::new();
+                for tri in indices {
+                    for &(pos, uv, _) in tri {
+                        vertices.push(Vertex {
+                            pos: object.position()[pos].into(),
+                            uv: object.texture()[uv.unwrap()].into()
+                        });
+                    }
+                }
+                vertices
+            },
+            start_time: Instant::now(),
+            prof: hprof::Profiler::new("Rasterizer")
+        }
+    }
+
+    fn render<C: gfx::CommandBuffer<R>>(&mut self, encoder: &mut gfx::Encoder<R, C>) {
+        self.prof.start_frame();
+
+        let elapsed = self.start_time.elapsed();
+        let time = elapsed.as_secs() as f32 + elapsed.subsec_nanos() as f32 / 1000_000_000.0;
+        let x = (time / 5.0).sin();
+        let y = (time / 5.0).cos();
+        let view = Transform::look_at(
+            Point3::new(x * 32.0,  16.0, y* 32.0),
+            Point3::new(0.0, 0.0, 0.0),
+            Vector3::unit_z(),
+        );
+
+        //self.rasterizer.set_proj(cgmath::ortho(-(SIZE.0 as f32) / 2.0, SIZE.0 as f32 / 2.0, SIZE.1 as f32 / 2.0, -(SIZE.0 as f32) / 2.0, 0.1, 1000.0));
+        self.rasterizer.set_proj(cgmath::perspective(cgmath::Deg(60.0f32), (SIZE.0 as f32 / SIZE.1 as f32), 0.1, 1000.0));
+        //self.rasterizer.set_proj(Matrix4::identity());
+        self.rasterizer.set_view(view);
+
+        self.prof.enter_noguard("clear");
+        self.rasterizer.clear();
+        self.prof.leave();
+
+        self.prof.enter_noguard("draw");
+        self.rasterizer.draw(&mut self.pipeline, &self.model, &self.prof);
+        self.prof.leave();
+
+        self.prof.enter_noguard("blit");
+        encoder.update_texture::<gfx::format::R8_G8_B8_A8, gfx::format::Rgba8>(&self.blit_texture, None, gfx::texture::NewImageInfo {
+            xoffset: 0,
+            yoffset: 0,
+            zoffset: 0,
+            width: SIZE.0 as _,
+            height: SIZE.1 as _,
+            depth: 0,
+            format: (),
+            mipmap: 0 as _
+        }, self.rasterizer.backbuffer()).unwrap();
+        self.prof.leave();
+
+        encoder.clear(&self.bundle.data.out, [0.3, 0.3, 0.3, 1.0]);
+        self.bundle.encode(encoder);
+
+        self.prof.end_frame();
+        if true {
+            self.prof.print_timing();
+        }
+    }
+}
+
+pub fn main() {
+    use gfx_app::Application;
+
+    App::launch_default("sw-rast");
+}
---
+++ src/rasterizer.rs
@@ -0,0 +1,269 @@
+use cgmath;
+use cgmath::SquareMatrix;
+use cgmath::{Matrix4, Vector4, Vector3, Vector2};
+
+use hprof;
+
+use std::f32;
+use std::mem;
+
+pub struct Rasterizer {
+    width: u32,
+    height: u32,
+    backbuffer: Vec<[u8; 4]>,
+    depthbuffer: Vec<u32>,
+    view: Matrix4<f32>,
+    proj: Matrix4<f32>
+}
+
+impl Rasterizer {
+    pub fn new(width: u32, height: u32) -> Self {
+        Rasterizer {
+            width: width,
+            height: height,
+            backbuffer: vec![[0; 4]; (width * height) as usize],
+            depthbuffer: vec![0; (width * height) as usize],
+            view: Matrix4::<f32>::identity(),
+            proj: Matrix4::<f32>::identity(),
+        }
+    }
+
+    pub fn draw<I, O>(&mut self, pipeline: &mut Pipeline<I, O>, vertices: &[I], prof: &hprof::Profiler)
+            where I: Copy + Clone, O: Copy + Clone + Blend
+    {
+        pipeline.process(self.width,
+                         self.height,
+                         vertices,
+                         self.view,
+                         self.proj,
+                         &mut self.backbuffer,
+                         &mut self.depthbuffer,
+                         &prof);
+    }
+
+    pub fn set_view(&mut self, mat: cgmath::Matrix4<f32>) {
+        self.view = mat;
+    }
+
+    pub fn set_proj(&mut self, mat: cgmath::Matrix4<f32>) {
+        self.proj = mat;
+    }
+
+    pub fn clear(&mut self) {
+        for x in &mut self.backbuffer {
+            x[0] = 0;
+            x[1] = 0;
+            x[2] = 0;
+            x[3] = 0;
+        }
+
+        for x in &mut self.depthbuffer {
+            *x = u32::max_value();
+        }
+    }
+
+    pub fn backbuffer(&self) -> &Vec<[u8; 4]> {
+        &self.backbuffer
+    }
+
+    pub fn depthbuffer(&self) -> &Vec<[u8; 4]> {
+        unsafe { mem::transmute(&self.depthbuffer) }
+    }
+
+}
+
+pub trait Blend {
+    fn blend(a: Self, aw: f32, b: Self, bw: f32, c: Self, cw: f32) -> Self;
+}
+
+struct Depth(f32);
+
+impl Blend for Depth {
+    fn blend(a: Self, aw: f32, b: Self, bw: f32, c: Self, cw: f32) -> Self {
+        Depth(a.0 * aw + b.0 * bw + c.0 * cw)
+    }
+}
+
+pub type VertexShader<I, O> = fn(I, Matrix4<f32>) -> (O, Vector4<f32>);
+pub type FragmentShader<I> = fn(Vector2<f32>, I) -> Vector4<f32>;
+
+pub struct Pipeline<I, O> {
+    vertex_fn: VertexShader<I, O>,
+    frag_fn: FragmentShader<O>,
+
+    frag_cache: Vec<O>,
+    vertex_cache: Vec<Vector4<f32>>
+}
+
+impl<I, O> Pipeline<I, O>
+        where I: Copy + Clone, O: Copy + Clone + Blend {
+    pub fn new(vertex: VertexShader<I, O>, frag: FragmentShader<O>) -> Self {
+        Pipeline {
+            vertex_fn: vertex,
+            frag_fn: frag,
+
+            frag_cache: Vec::with_capacity(4096),
+            vertex_cache: Vec::with_capacity(4096)
+        }
+    }
+
+    #[inline]
+    fn rasterize(width: u32,
+                 height: u32,
+                 triangles: &Vec<Vector4<f32>>,
+                 attributes: &Vec<O>,
+                 backbuffer: &mut Vec<[u8; 4]>,
+                 depth: &mut Vec<u32>,
+                 frag: FragmentShader<O>)
+    {
+        let width = width as i32;
+        let height = height as i32;
+
+        #[inline]
+        fn edge_function(a: Vector2<i32>, b: Vector2<i32>, c: Vector2<i32>) -> i32 {
+            (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x)
+        }
+
+        #[inline]
+        fn edge_step(a: Vector2<i32>, b: Vector2<i32>, step: i32) -> i32 {
+            -(b.y-a.y)*step
+        }
+
+        #[inline]
+        fn map_coord(vec: Vector4<f32>, width: f32, height: f32, near: f32) -> Vector2<i32> {
+            let screen = Vector2::new(vec.x / -vec.z, vec.y / -vec.z);
+            let ndc = Vector2::new(2f32 * screen.x / width - 1f32,
+                                   2f32 * screen.y / height - 1f32);
+            Vector2::new(((screen.x + 1f32) / 2f32 * width) as i32,
+                         ((1f32 - screen.y) / 2f32 * height) as i32)
+        }
+
+        let step = 256;
+        let mask = step - 1;
+
+        for triangle in (triangles.chunks(3).zip(attributes.chunks(3))).into_iter() {
+            if let (&[a, b, c], &[A, B, C]) = triangle {
+                // TODO: return coords as super-sampled coords in 1/256th
+                //       resolution
+
+                let p1 = map_coord(a, width as f32, height as f32, 0.1f32);
+                let p2 = map_coord(b, width as f32, height as f32, 0.1f32);
+                let p3 = map_coord(c, width as f32, height as f32, 0.1f32);
+
+                let mut min = Vector2::<i32>::new(i32::max_value(), i32::max_value());
+                let mut max = Vector2::<i32>::new(i32::min_value(), i32::min_value());
+
+                use std::cmp;
+
+                min.x = cmp::min(cmp::min(cmp::min(min.x, p1.x), p2.x), p3.x);
+                min.y = cmp::min(cmp::min(cmp::min(min.y, p1.y), p2.y), p3.y);
+
+                max.x = cmp::max(cmp::max(cmp::max(min.x, p1.x), p2.x), p3.x);
+                max.y = cmp::max(cmp::max(cmp::max(min.y, p1.y), p2.y), p3.y);
+
+                /*if (p1.x as i32) < min.x { min.x = p1.x as i32; }
+                if (p1.y as i32) < min.y { min.y = p1.y as i32; }
+                if (p2.x as i32) < min.x { min.x = p2.x as i32; }
+                if (p2.y as i32) < min.y { min.y = p2.y as i32; }
+                if (p3.x as i32) < min.x { min.x = p3.x as i32; }
+                if (p3.y as i32) < min.y { min.y = p3.y as i32; }
+
+                if (p1.x as i32) > max.x { max.x = p1.x as i32; }
+                if (p1.y as i32) > max.y { max.y = p1.y as i32; }
+                if (p2.x as i32) > max.x { max.x = p2.x as i32; }
+                if (p2.y as i32) > max.y { max.y = p2.y as i32; }
+                if (p3.x as i32) > max.x { max.x = p3.x as i32; }
+                if (p3.y as i32) > max.y { max.y = p3.y as i32; }
+*/
+                if max.x > width  { max.x = width; }
+                if max.y > height { max.y = height; }
+                if min.x > width  { min.x = width; }
+                if min.y > height { min.y = height; }
+
+                if max.x < 0 { max.x = 0; }
+                if max.y < 0 { max.y = 0; }
+                if min.x < 0 { min.x = 0; }
+                if min.y < 0 { min.y = 0; } 
+
+                //println!("({}, {}), ({}, {})", min.x, min.y, max.x, max.y);
+                /*min.x = (min.x + mask) & !mask;
+                min.y = (min.y + mask) & !mask;
+                max.x = (max.x + mask) & !mask;
+                max.y = (max.y + mask) & !mask;*/
+
+                let (mut w1, mut w2, mut w3) = (edge_function(p1, p2, point),
+                                                edge_function(p2, p3, point),
+                                                edge_function(p3, p1, point));
+
+                let (w1_step, w2_step, w3_step) = (edge_step(p1, p2, 1),
+                                                   edge_step(p2, p3, 1),
+                                                   edge_step(p3, p1, 1));
+
+                for x in (min.x..max.x) {
+                    for y in (min.y..max.y) {
+                        let point = Vector2::new(x, y);
+
+                        let area = edge_function(p1, p2, p3);
+                        if area <= 0 {
+                            continue;
+                        }
+
+                        if w1 >= 0 && w2 >= 0 && w3 >= 0 {
+                            //w1 /= area;
+                            //w2 /= area;
+                            //w3 /= area;
+
+                            let depth_val = ((f32::min(Blend::blend(Depth(-a.z), w1 as f32, Depth(-b.z), w2 as f32, Depth(-c.z), w3 as f32).0,
+                                                       1000f32)
+                                                     / 1000f32) * u32::max_value() as f32) as u32;
+
+                            if depth_val < depth[(x + width * y ) as usize] {
+                                depth[(x + width * y) as usize] = depth_val;
+
+                                let color = (frag)(Vector2::new(point.x as f32, point.y as f32), Blend::blend(A, w1 as f32, B, w2 as f32, C, w3 as f32));
+
+                                backbuffer[(x + width * y) as usize][0] = (color.x * 255f32) as u8;
+                                backbuffer[(x + width * y) as usize][1] = (color.y * 255f32) as u8;
+                                backbuffer[(x + width * y) as usize][2] = (color.z * 255f32) as u8;
+                                backbuffer[(x + width * y) as usize][3] = (color.w * 255f32) as u8;
+                            }
+                        }
+
+                        w1 += w1_step;
+                        w2 += w2_step;
+                        w3 += w3_step;
+                    }
+
+                    w1 += w1_step;
+                    w2 += w2_step;
+                    w3 += w3_step;
+                }
+            }
+        }
+    }
+
+    pub fn process(&mut self, width: u32, height: u32, vertices: &[I], view: Matrix4<f32>, proj: Matrix4<f32>, mut backbuffer: &mut Vec<[u8; 4]>, mut depth: &mut Vec<u32>, prof: &hprof::Profiler) {
+        prof.enter_noguard("transform");
+        for &vertex in vertices {
+            let (out, pos) = (self.vertex_fn)(vertex, proj * view * Matrix4::from_scale(15f32));
+
+            self.frag_cache.push(out);
+            self.vertex_cache.push(pos);
+        }
+        prof.leave();
+
+        // borrowck can't see iterator borrow as disjoint from rest
+        prof.enter_noguard("rasterize");
+        Self::rasterize(width,
+                        height,
+                        &self.vertex_cache,
+                        &self.frag_cache,
+                        &mut backbuffer,
+                        &mut depth,
+                        self.frag_fn);
+        prof.leave();
+
+        self.frag_cache.clear();
+        self.vertex_cache.clear();
+    }
+}
